b89d0d9415e1c88274f82de1d0e3c8177c3fb81b
“Corinne <corinne.canavan@gmail.com>
2022-08-03 15:13:31 -0600

    Add functionality to fetch requests and invoke them within useEffect

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
 import { Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   const [ingredients, setIngredients] = useState([])
-  
+  console.log("ingredients", ingredients)
+
+  const getMaterialIngredients = () => {
+    fetch('https://botw-compendium.herokuapp.com/api/v2/category/materials')
+    .then(response => response.json())
+    .then(data => setIngredients(data.data))
+    .catch(error => console.log(error))
+  }
+
+  const getCreatureIngredients = () => {
+    fetch('https://botw-compendium.herokuapp.com/api/v2/category/creatures')
+    .then(response => response.json())
+    .then(data => setIngredients(prevState => [...prevState, ...data.data.food]))
+    .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    getMaterialIngredients()
+    getCreatureIngredients()
+  }, [])
+
   return (
     <main className="main-container">
       <header>
