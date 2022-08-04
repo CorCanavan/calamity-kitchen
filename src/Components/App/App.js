59c7f1ddc2dbeca2edd9bb98d95ae725598573fe
“Corinne <corinne.canavan@gmail.com>
2022-08-03 20:30:13 -0600

    Add functionality to onChange and onClick

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,6 +9,8 @@ const App = () => {
   const [ingredients, setIngredients] = useState([])
   console.log("ingredients", ingredients)
 
+  const [cookingEffect, setCookingEffect] = useState('')
+
   const getMaterialIngredients = () => {
     fetch('https://botw-compendium.herokuapp.com/api/v2/category/materials')
     .then(response => response.json())
@@ -28,13 +30,18 @@ const App = () => {
     getCreatureIngredients()
   }, [])
 
+  const filterIngredients = () => {
+    const filteredIngredients = ingredients.filter(ingredient => ingredient.cooking_effect === cookingEffect)
+    setIngredients(filteredIngredients)
+  }
+
   return (
     <main className="main-container">
       <header>
         <h1 className="header-title">Calamity Kitchen</h1>
       </header>
       <Route exact path="/" >
-        <Dropdown ingredients={ingredients} />
+        <Dropdown ingredients={ingredients} cookingEffect={cookingEffect} setCookingEffect={setCookingEffect} filterIngredients={filterIngredients} />
         <Ingredients ingredients={ingredients} />
       </Route>
     </main>
--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Dropdown.css';
 
-const Dropdown = ({ ingredients }) => {
+const Dropdown = ({ ingredients, cookingEffect, setCookingEffect, filterIngredients }) => {
 
   // const allCookingEffects = ingredients.map(ingredient => ingredient.cooking_effect)
   // console.log("allCE", allCookingEffects);
@@ -11,23 +11,30 @@ const Dropdown = ({ ingredients }) => {
       if (!acc.includes(ingredient.cooking_effect) && ingredient.cooking_effect) {
       acc.push(ingredient.cooking_effect)
       }
+      // console.log("acc", acc)
       return acc;
     }, [])
-    .sort((a, b) => a.localeCompare(b))
-    .map(cookingEffect => {
+    .sort()
+    .map(cookEffect => {
       return (
-        <option value={cookingEffect} key={cookingEffect}>{cookingEffect}</option>
+        <option value={cookEffect} key={cookEffect}>{cookEffect}</option>
       )
     })
 
   console.log("reduce", cookingEffects)
 
+  const handleClick = (e) => {
+    e.preventDefault()
+    filterIngredients()
+  }
+
   return (
     <form>
-      <select>
+      <select onChange={(e) => setCookingEffect(e.target.value)}>
         <option value="select">Select a Cooking Effect</option>
           {cookingEffects}
       </select>
+      <button className="get-ingredients" onClick={(e) => handleClick(e)}>Get Ingredients!</button>
     </form>
   )
 }
