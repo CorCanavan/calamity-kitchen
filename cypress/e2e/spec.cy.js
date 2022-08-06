d34f145c3096229ed7d29cde77fb77e8082bd9d9
“Corinne <corinne.canavan@gmail.com>
2022-08-05 22:11:39 -0600

    Add test that correct elements render to homepage on page load

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,7 +12,17 @@ describe('Homepage user flows', () => {
     })
     cy.visit('http://localhost:3000/')
   })
-  it('passes', () => {
-    // cy.visit('http://localhost:3000/')
+
+  it('should render the site header, dropdown form, and all ingredient cards on page load, ', () => {
+    cy.get('.header-title').contains('h1', 'Calamity Kitchen')
+
+    cy.get('form').find('select').should('have.length', 1)
+    cy.get('option[value="select"]').should('contain', 'Select a Cooking Effect')
+
+    cy.get('.ingredients-container').find('.card').should('have.length', 4)
+    cy.get('.card').first().contains('h2', 'blue nightshade')
+    cy.get('.card').first().find('img').should('have.attr', 'src', 'https://botw-compendium.herokuapp.com/api/v2/entry/blue_nightshade/image')
   })
+
+
 })
