5926016a9d934b25284f3319c1fd40cfc538104a
“Corinne <corinne.canavan@gmail.com>
2023-02-22 14:09:14 -0700

    Separate header into it's own component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+const Header = () => {
+
+  return (
+    <header>
+        <Link to="/">
+          <h1 className="header-title">Calamity Kitchen</h1>
+        </Link>
+      </header>
+  )
+}
+
+export default Header;
