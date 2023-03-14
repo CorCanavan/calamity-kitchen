32a9d396ba17a1ed699858d50c80b706108d23db
“Corinne <corinne.canavan@gmail.com>
2023-03-14 14:27:41 -0600

    Create shell of Footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => {
+
+  return (
+    <footer>
+      <section className="createdBy-info">
+        <h3>Created By:</h3>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer;
