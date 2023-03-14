61b0f5adab56cf8de4587c2e1d725cad29b80156
“Corinne <corinne.canavan@gmail.com>
2023-03-14 16:38:23 -0600

    Clean up Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,20 +7,9 @@ const Footer = () => {
       <section className="createdBy-info">
         <h3>Created By:</h3>
         <p className="footer-name">Corinne Canavan</p>
-        {/* <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub</a>
-        <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
-        <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail</a> */}
-      </section>
-      <section className="contact-info">
-        <h3>Contact:</h3>
-        <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub</a>
-        <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail</a>
-        <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
-      </section>
-      <section className="resources-info">
-        <h3>Resources:</h3>
-        <a className="resources-link" href="https://gadhagod.github.io/Hyrule-Compendium-API/#/">Hyrule Compendium API</a>
-        <a className="resources-link" href="https://artsyomni.com/hyliaserif">Hylia Serif Font</a>
+        <span className="contact-info"><a className="createdBy-link" href="https://github.com/CorCanavan">GitHub | </a>
+        <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail | </a>
+        <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a></span>
       </section>
     </footer>
   )
