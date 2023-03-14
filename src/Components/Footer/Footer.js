961f88661750c2447aa20306698ff6f0cd00d5c3
“Corinne <corinne.canavan@gmail.com>
2023-03-14 16:06:34 -0600

    Adjust content

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,9 +7,15 @@ const Footer = () => {
       <section className="createdBy-info">
         <h3>Created By:</h3>
         <p className="footer-name">Corinne Canavan</p>
-        <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub</a>
+        {/* <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub</a>
         <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
+        <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail</a> */}
+      </section>
+      <section className="contact-info">
+        <h3>Contact:</h3>
+        <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub</a>
         <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail</a>
+        <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
       </section>
       <section className="resources-info">
         <h3>Resources:</h3>
