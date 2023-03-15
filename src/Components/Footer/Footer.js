830b69311574522b6787a14802fd5356ae082f62
“Corinne <corinne.canavan@gmail.com>
2023-03-14 18:33:25 -0600

    Remove commented out code

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,13 +5,12 @@ const Footer = () => {
   return (
     <footer>
       <section className="createdBy-info">
-        {/* <h3>Created By:</h3>
-        <p className="footer-name">Corinne Canavan</p> */}
-        {/* <span className="created-by"> <h3>Created By:</h3> Corinne Canavan</span> */}
         <p className="created-by">Created By: <span className="footer-name">Corinne Canavan</span></p>
-        <span className="contact-info"><a className="createdBy-link" href="https://github.com/CorCanavan">GitHub | </a>
-        <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail | </a>
-        <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a></span>
+        <div className="contact-info">
+          <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub | </a>
+          <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail | </a>
+          <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
+        </div>
       </section>
     </footer>
   )
