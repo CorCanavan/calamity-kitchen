abceca8aab8b53432b9bbc03d6529c341b717ca9
“Corinne <corinne.canavan@gmail.com>
2023-03-14 18:38:30 -0600

    Update classNames to have consistent casing

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,12 +4,12 @@ const Footer = () => {
 
   return (
     <footer>
-      <section className="createdBy-info">
+      <section className="created-by-info">
         <p className="created-by">Created By: <span className="footer-name">Corinne Canavan</span></p>
         <div className="contact-info">
-          <a className="createdBy-link" href="https://github.com/CorCanavan">GitHub | </a>
-          <a className="createdBy-link" href="mailto:corinne.canavan@gmail.com">Gmail | </a>
-          <a className="createdBy-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
+          <a className="info-link" href="https://github.com/CorCanavan">GitHub | </a>
+          <a className="info-link" href="mailto:corinne.canavan@gmail.com">Gmail | </a>
+          <a className="info-link" href="https://www.linkedin.com/in/corinnecanavan/">LinkedIn</a>
         </div>
       </section>
     </footer>
