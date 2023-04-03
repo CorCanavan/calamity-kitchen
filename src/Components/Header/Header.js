917de706190b1d7556cf5439757201bbb65bf4ec
“Corinne <corinne.canavan@gmail.com>
2023-04-03 14:32:26 -0600

    Add Home to Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,16 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
 
   return (
     <header>
+      <div className="nav-link-container">
+          <NavLink to="/home">
+            <h2>Home</h2>
+          </NavLink>
+        </div>
         <Link to="/home">
           <h1 className="header-title">Calamity Kitchen</h1>
         </Link>
