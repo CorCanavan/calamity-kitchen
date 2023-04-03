a009904b1494030d8c5d11f52cd507bc390162f1
“Corinne <corinne.canavan@gmail.com>
2023-04-03 15:54:31 -0600

    Add Recipes to Header; adjust CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,14 +6,24 @@ const Header = () => {
 
   return (
     <header>
+      <div className="title-container">
+          <Link to="/home">
+            <h1 className="header-title">Calamity Kitchen</h1>
+          </Link>
+      </div>
       <div className="nav-link-container">
           <NavLink to="/home">
-            <h2>Home</h2>
+            <h2 className="nav-links">Home</h2>
+          </NavLink>
+          <NavLink to="/recipes">
+            <h2 className="nav-links">Recipes</h2>
           </NavLink>
         </div>
-        <Link to="/home">
-          <h1 className="header-title">Calamity Kitchen</h1>
-        </Link>
+        {/* <div className="title-container">
+          <Link to="/home">
+            <h1 className="header-title">Calamity Kitchen</h1>
+          </Link>
+        </div> */}
       </header>
   )
 }
