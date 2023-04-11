443e4b28f52c4d815a33cb67589293f34567bf58
“Corinne <corinne.canavan@gmail.com>
2023-04-11 15:53:37 -0600

    Add media query for img

--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -1,11 +1,13 @@
 import './Recipes.css';
+import sitByFire from '../../assets/sitByFire3.png';
 
 const Recipes = () => {
 
   return (
     <section className="recipes-page">
-      <h2 className="coming-soon">Coming Soon - Recipes Page!</h2>
+      <h2 className="coming-soon">Coming Soon: Recipes Page!</h2>
       <p className="recipes-blurb">A place to store your favorite recipes for easy in-game reference.</p>
+      <img className="link-by-fire" src={sitByFire} alt="Link waiting by fire"/>
     </section>
   )
 }
