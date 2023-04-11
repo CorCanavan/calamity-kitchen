fd9a2a2d85bbde53459d3980916315bc9e167557
“Corinne <corinne.canavan@gmail.com>
2023-04-11 14:45:12 -0600

    Create shell of Recipes Page with blurb

---
+++ src/Components/Recipes/Recipes.js
@@ -0,0 +1,13 @@
+import './Recipes.css';
+
+const Recipes = () => {
+
+  return (
+    <section className="recipes-page">
+      <h2>Coming Soon - Recipes Page!</h2>
+      <p>A place to store your favorite recipes for easy in-game reference.</p>
+    </section>
+  )
+}
+
+export default Recipes;
