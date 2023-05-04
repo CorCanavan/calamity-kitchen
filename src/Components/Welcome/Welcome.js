4afd0cb8299723e3caba642cf0554876602456c6
“Corinne <corinne.canavan@gmail.com>
2023-05-04 13:54:40 -0600

    Break paragraph into 2

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -12,7 +12,10 @@ const Welcome = () => {
           In the video game <i>Zelda: Breath of the Wild</i>, there’s enough to worry about between freeing the Divine Beasts and defeating Calamity Ganon, so we’re here to take the stress out of cooking in the game! 
           </p><br></br>
           <p className="welcome-blurb">
-          If you’ve played the game before, you may know that cooking can yield you a plentiful meal with strong effects, or you may end up with the dreaded “dubious food” - a concoction with no beneficial effects that causes Link to squirm as he eats! Click the button below to use the dropdown to filter ingredients by effect. Cooking ingredients together with similar effects produces meals with a powerful boost. Don't know the effect? No problem! Use the search bar to find an ingredient by name. Click on an ingredient card for more details - where to find it in the game, how many hearts it restores, and more.
+          If you’ve played the game before, you may know that cooking can yield you a plentiful meal with strong effects, or you may end up with the dreaded “dubious food” - a concoction with no beneficial effects that causes Link to squirm as he eats!
+          </p><br></br>
+          <p className="welcome-blurb">
+          Click the button below to use the dropdown to filter ingredients by effect. Cooking ingredients together with similar effects produces meals with a powerful boost. Don't know the effect? No problem! Use the search bar to find an ingredient by name. Click on an ingredient card for more details - where to find it in the game, how many hearts it restores, and more.
           </p>
         </div>
         <div className="gif-wrapper">
